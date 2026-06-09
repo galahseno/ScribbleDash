c4305631022b29a51161b275f2be0615404c1c89
galahseno <galahsenoadjie@gmail.com>
2026-06-09 20:58:52 +0700

    Initial Commit

---
+++ ScribbleDash/ScribbleDashApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScribbleDashApp.swift
+//  ScribbleDash
+//
+//  Created by Galah Seno on 09/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ScribbleDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
